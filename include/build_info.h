28f39df414e1db0c32cdfbce30af46f6aa96363d
Christoph Lechner <christoph.lechner@desy.de>
2023-07-27 14:49:49 +0200

    remove files no longer required for CMake-based build process

--- include/build_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BUILD_INFO_H
-#define __BUILD_INFO_H
-
-#ifdef __cplusplus
-extern "C" /* effective only for C++ compilers, declaration continues after #endif */
-#endif
-const char *build_info(void);
-
-#endif
