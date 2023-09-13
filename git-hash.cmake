795e7a1186a8e0ce95b1464ea52196c2c848b1fa
reiche <sven.reiche@psi.ch>
2023-09-13 16:37:42 +0200

    Prepare dev branch for version 4.6.4-beta

--- git-hash.cmake
+++ git-hash.cmake
@@ -14,8 +14,8 @@ endif()
 message(STATUS "Git hash is ${GIT_HASH}")
 set(_GENESIS_VERSION_MAJOR "4")
 set(_GENESIS_VERSION_MINOR "6")
-set(_GENESIS_VERSION_REV "3")
-set(_GENESIS_VERSION_BETA "false")
+set(_GENESIS_VERSION_REV "4")
+set(_GENESIS_VERSION_BETA "true")
 execute_process(
         COMMAND
             whoami
