19c278eafe359c8ea0adbe07aab452157fbf204e
Christoph Lechner <christoph.lechner@desy.de>
2023-09-27 01:26:11 +0200

    files from ba17c40

--- include/DiagnosticPlugin.h
+++ include/DiagnosticPlugin.h
@@ -4,9 +4,16 @@
 // header file with definitions of data structures for information exchange
 #include "DiagnosticHookS.h"
 
+class DiagBeamHookedBase {
+public:
+	virtual ~DiagBeamHookedBase() {};
+
+	virtual bool get_infos(DiagBeamHookInfos *) = 0;
+	virtual void doit(DiagBeamHookData *) = 0;
+};
+
 class DiagFieldHookedBase {
 public:
-//	DiagFieldHookedBase();
 	virtual ~DiagFieldHookedBase() {};
 
 	virtual bool get_infos(DiagFieldHookInfos *) = 0;
