3499f2f972e118c224437745432cd7cdbe09c1df
reiche <sven.reiche@psi.ch>
2025-06-26 10:17:20 +0200

    Fix of Issue 225 (cleaner implementation)

--- src/Loading/ShotNoise.cpp
+++ src/Loading/ShotNoise.cpp
@@ -39,15 +39,20 @@ void ShotNoise::applyShotNoise(Particle *beam, int npart, int nbins, double ne)
   int mpart=npart/nbins;
 
   double nbl=ne/static_cast<double>(mpart);  // number of simulated electrons per beamlet 
-  if (nbl < 10) {nbl = 10;}  // check for case when there are more macro particles then electrons to be simulated.
+  if (nbl < 1) {nbl = 1;}  // check for case when there are more macro particles then electrons to be simulated.
 
   for (int i=0; i< npart; i++){
     work[i]=0;
   }
+  double twopi = 4*asin(1.);
+
   for (int ih=0; ih< (nbins-1)/2; ih++){
     for (int i1=0;i1<mpart;i1++){
       double phi=4*asin(1)*sran->getElement();
       double an=sqrt(-log(sran->getElement())/nbl)*2/static_cast<double>(ih+1);
+      if (an > twopi) {
+          an = fmod(an,twopi);
+      }
       for (int i2=0; i2<nbins; i2++){
         int idx=i1*nbins+i2;
         work[idx]-=an*sin(beam[idx].theta*(ih+1)+phi);
